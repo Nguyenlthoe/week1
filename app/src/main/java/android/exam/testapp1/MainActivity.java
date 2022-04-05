ba799fe81af8a542a08fc7e576524465d3adf0ce
Nguyenlthoe <nguyenlthoe@gmail.com>
2022-04-05 11:40:54 +0700

    Initial commit

---
+++ app/src/main/java/android/exam/testapp1/MainActivity.java
@@ -0,0 +1,23 @@
+package android.exam.testapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textct = findViewById(R.id.textcenter);
+                textct.setText("xin chào");
+            }
+        });
+    }
+}
